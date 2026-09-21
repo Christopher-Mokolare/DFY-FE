a6c2e8dd38e3382559bbe1e6631605ce1c82adfb
OBAKENG <2co.mokolare@gmail.com>
2026-09-21 18:12:31 +0200

    feat: route creator action queue

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import PostErrand from './pages/tasks/PostErrand'
 import MyPostedTasks from './pages/tasks/MyPostedTasks'
 import MyActiveTasks from './pages/tasks/MyActiveTasks'
 import MyCompletedTasks from './pages/tasks/MyCompletedTasks'
+import ActionRequired from './pages/tasks/ActionRequired'
 import TaskChat from './pages/tasks/TaskChat'
 import Notifications from './pages/Notifications'
 import Profile from './pages/user/Profile'
@@ -48,6 +49,7 @@ export default function App() {
               <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
               <Route path="/tasks/post" element={<RequireAuth><PostErrand /></RequireAuth>} />
               <Route path="/tasks/my-posted" element={<RequireAuth><MyPostedTasks /></RequireAuth>} />
+              <Route path="/tasks/action-required" element={<RequireAuth><ActionRequired /></RequireAuth>} />
               <Route path="/tasks/my-active" element={<RequireRunnerAccess><MyActiveTasks /></RequireRunnerAccess>} />
               <Route path="/tasks/my-completed" element={<RequireAuth><MyCompletedTasks /></RequireAuth>} />
               <Route path="/tasks/:taskId/chat" element={<RequireAuth><TaskChat /></RequireAuth>} />
